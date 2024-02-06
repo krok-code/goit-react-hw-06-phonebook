f7b4d57b9bf585f9db9c5d5e78a0a6a7530a7fb0
krok-code <maks.smolinskyi@gmail.com>
2024-02-06 21:45:22 +0200

    Update useLocaleStorage.jsx

--- src/components/hooks/useLocaleStorage.jsx
+++ src/components/hooks/useLocaleStorage.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 
 export default function useLocaleStorage(key, defaultValue) {
-  const [state, setState] = useState(() => {
+  const [contact, setState] = useState(() => {
     return JSON.parse(window.localStorage.getItem(key) ?? defaultValue);
   });
 
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
-  return [state, setState];
+    window.localStorage.setItem(key, JSON.stringify(contact));
+  }, [key, contact]);
+  return [contact, setState];
 }
