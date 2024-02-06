7adba3068ccd9d215399d6fafb20e8ae74c22ef5
krok-code <maks.smolinskyi@gmail.com>
2024-02-06 15:55:38 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from './Layout.styled';
+import { Container } from './Layout.styled.css';
 import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => {
